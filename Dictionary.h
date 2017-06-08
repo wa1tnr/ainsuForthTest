fe94d330fb27546789c8364dc8714cb224ab69d0
wa1tnr <wa1tnr@users.noreply.github.com>
2017-06-08 03:01:32 +0000

    Initial commit.

---
+++ Dictionary.h
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - -
+// - - - -   the  switchyard   - - - -
+// - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - -
+  
+// #include "src/kernel/quit.h"
+#include "src/kernel/do_sys.h"
+#include "src/kernel/dot_quote.h"
+// #include "src/kernel/jump.h"
+// #include "src/kernel/leave_sys.h"
+// #include "src/kernel/loop_sys.h"
+// #include "src/kernel/plus_loop_sys.h"
+#include "src/kernel/quit.h"
+// #include "src/kernel/subroutine.h"
+// #include "src/kernel/zjump.h"
+
+ 27 /**  File: YAFFA-ARM.ino                                                            **/
+ 28 "../ainsuForth/Dictionary.h"
+
